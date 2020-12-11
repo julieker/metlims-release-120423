47883fbebb799c15275d8cd51641647b0c09b60e
admin <admin@admins-MacBook-Pro.local>
2020-12-11 16:37:18 -0500

    mixtures -- issue 94 and 107 -- add in compound name to mixture detail and have link to view aliquot

--- lipidstest/src/main/java/edu/umich/brcf/metabolomics/panels/lims/mixtures/MixtureAliquotDetail.java
+++ lipidstest/src/main/java/edu/umich/brcf/metabolomics/panels/lims/mixtures/MixtureAliquotDetail.java
@@ -1,7 +1,10 @@
 package edu.umich.brcf.metabolomics.panels.lims.mixtures;
 
 import java.util.List;
+
+import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
@@ -12,8 +15,12 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import edu.umich.brcf.metabolomics.layers.domain.Compound;
+import edu.umich.brcf.metabolomics.layers.domain.CompoundName;
+import edu.umich.brcf.metabolomics.layers.service.CompoundNameService;
 import edu.umich.brcf.metabolomics.layers.service.CompoundService;
 import edu.umich.brcf.metabolomics.layers.service.InventoryService;
+import edu.umich.brcf.metabolomics.panels.lims.compounds.EditAliquot;
+import edu.umich.brcf.metabolomics.panels.lims.compounds.InventoryDetailPanel;
 import edu.umich.brcf.shared.layers.domain.Aliquot;
 import edu.umich.brcf.shared.layers.domain.Inventory;
 import edu.umich.brcf.shared.layers.domain.Mixture;
@@ -37,6 +44,8 @@ public class MixtureAliquotDetail extends WebPage
 	InventoryService inventoryService;
 	@SpringBean
 	UserService userService;
+	@SpringBean
+	CompoundNameService compoundNameService;
 	Mixture mixture;
 	ListView listView;
 	WebMarkupContainer container;
@@ -69,7 +78,8 @@ public class MixtureAliquotDetail extends WebPage
 				final Aliquot alq = (Aliquot) listItem.getModelObject();	
 			    MixtureAliquotPK mixtureAliquotPK   = MixtureAliquotPK.instance(mixture, alq);	
 			    MixtureAliquot mixtureAliquot = mixtureService.loadMixtureAliquotById(mixtureAliquotPK);				
-			    listItem.add(new Label("aliquotId", new Model(alq.getAliquotId())));
+			    listItem.add(buildAliquotAjaxLink("aliquotLink", alq, modal2 ));
+			    listItem.add(new Label("aliquotName", new Model(getCompoundName(alq))));
 				listItem.add(new Label("aliquotVolume", new Model(mixtureAliquot.getVolumeAliquot())));
 				listItem.add(new Label("aliquotConcentrate", new Model(mixtureAliquot.getConcentrationAliquot())));				
 				}
@@ -80,6 +90,48 @@ public class MixtureAliquotDetail extends WebPage
 		add(container);
         }
 		
+	private AjaxLink buildAliquotAjaxLink(String id, final Aliquot aliquot, final ModalWindow modal2)
+		{
+		AjaxLink link;		
+		// Issue 237
+		// issue 39
+		Compound cmpd  = aliquot.getCompound();
+		final InventoryDetailPanel idp = new InventoryDetailPanel ("inventory", cmpd);
+	    link =  new AjaxLink <Void>(id)
+			{
+			@Override
+			public void onClick(AjaxRequestTarget target) 
+				{
+				try
+					{
+					modal2.setInitialHeight(900);
+					modal2.setInitialWidth(1100);
+					modal2.setPageCreator(new ModalWindow.PageCreator()
+						{
+						public Page createPage()
+							{
+													
+							return new EditAliquot(getPage(), new Model <Aliquot> (aliquot),idp, modal2, true);
+							}
+						});
+					//issue 239
+					modal2.setWindowClosedCallback(new ModalWindow.WindowClosedCallback() 
+					    {
+					    @Override
+					    public void onClose(AjaxRequestTarget target)  
+					        {    
+					    	target.add(modal2.getParent());				    	
+					        }
+					    });
+					modal2.show(target);
+					}
+				catch (Exception e) {  }
+				}
+			};
+	    link.add(new Label("aliquotid", aliquot.getAliquotId()));
+		return link;
+		}
+	
 	private List<Inventory> invList;
 	public List<Inventory> getInvList()
 		{
@@ -105,5 +157,16 @@ public class MixtureAliquotDetail extends WebPage
 		{
 		List<Aliquot> nList = aliquotService.aliquotIdsForMixtureId(mixture.getMixtureId());
 		return nList;
+		}
+	
+	public String getCompoundName(Aliquot alq)
+		{
+		List<CompoundName> cnList = compoundNameService.loadByCid(alq.getCompound().getCid());
+		for (CompoundName cname : cnList)
+			{
+			if (cname.getNameType().equals("pri"))
+			    return cname.getName().substring(0, (cname.getName().length() >=50 ? 50 : cname.getName().length() ) );
+			}
+		return "";
 		}
 	}
