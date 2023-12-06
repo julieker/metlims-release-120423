63abfbad12ff50382f86f869dd1aea232fa38005
julieker <julieker@umich.edu>
2023-12-06 14:39:11 -0500

    fix color coding for preinjections to have same color coding as plate issue 313

--- lipidstest/src/main/java/edu/umich/brcf/metabolomics/panels/workflow/worklist_builder/WorklistFieldBuilder.java
+++ lipidstest/src/main/java/edu/umich/brcf/metabolomics/panels/workflow/worklist_builder/WorklistFieldBuilder.java
@@ -247,222 +247,221 @@ public class WorklistFieldBuilder implements Serializable
 	
 // issue 371 and 372 and 394
 	private static String shadeBackground(WorklistItemSimple item)
-		{     
-		String style = "background :";
-		String name = item.getSampleType().trim();
-		if (item.getSampleType().startsWith("Blank"))
-			style += "#CEF7E8";  
-		else if (item.getSampleType().startsWith("Pooled Plasma"))
-			style += "#F6CDCB"; 
-		else if (item.getSampleType().startsWith("Standard Mix"))
-			style += "#CBE3F6";  
-		else if (item.getSampleType().startsWith("Test Pool"))
-			style += "#D9F6CB";  
-		else if (item.getSampleType().startsWith("Neat Blank"))
-			style += "#CEF7E8"; 
-		// issue 255
-		else if (item.getSampleType().startsWith("Process Blank"))
-			style += "#F6CDCB";  
-		else if (item.getSampleType().startsWith("RC Plasma"))
-			style += "#CBE3F6";  
-		else if (item.getSampleType().startsWith("Red Cross"))
-			style += "#CBE3F6";  
-		else if (item.getSampleType().startsWith("Matrix Blank"))
-			style += "#CBE3F6";  	
-		else if (item.getSampleType().startsWith("Solvent Blank"))
-			style += "#D9F6CB";  
-		else if (item.getSampleType().startsWith("Standard.0"))
-			style += "#EFF6CB";  
-		else if (item.getSampleType().startsWith("Standard 0"))
-			style += "#EFF6CB";  
-		else if (item.getSampleType().startsWith("Standard.10"))
-			style += "#F6CBF2";  
-		else if (item.getSampleType().startsWith("Standard 10"))
-			style += "#F6CBF2";  
-		else if (item.getSampleType().startsWith("Standard.11"))
-			style += "#E9E9FF";
-		else if (item.getSampleType().startsWith("Standard 11"))
-			style += "#E9E9FF";
-		else if (item.getSampleType().startsWith("Standard.1"))
-			style += "#E9E9FF";  
-		else if (item.getSampleType().startsWith("Standard 1"))
-			style += "#E9E9FF";  
-		else if (item.getSampleType().startsWith("Standard.2"))
-			style += "#CBD9F6";  
-		else if (item.getSampleType().startsWith("Standard 2"))
-			style += "#CBD9F6";  
-		else if (item.getSampleType().startsWith("Standard.3"))
-			style += "#E8CBF6";  
-		else if (item.getSampleType().startsWith("Standard 3"))
-			style += "#E8CBF6";  
-		else if (item.getSampleType().startsWith("Standard.4"))
-			style += "#F6CBF2";  
-		else if (item.getSampleType().startsWith("Standard 4"))
-			style += "#F6CBF2";  
-		else if (item.getSampleType().startsWith("Standard.5"))
-			style += "#E9E9FF";
-		else if (item.getSampleType().startsWith("Standard 5"))
-			style += "#E9E9FF";
-		else if (item.getSampleType().startsWith("Standard.6"))
-			style += "#EFF6CB";  
-		else if (item.getSampleType().startsWith("Standard 6"))
-			style += "#EFF6CB";  
-		else if (item.getSampleType().startsWith("Standard.7"))
-			style += "#CBF6D2";  
-		else if (item.getSampleType().startsWith("Standard 7"))
-			style += "#CBF6D2";  
-		else if (item.getSampleType().startsWith("Standard.8"))
-			style += "#CBD9F6";  
-		else if (item.getSampleType().startsWith("Standard 8"))
-			style += "#CBD9F6";  
-		else if (item.getSampleType().startsWith("Standard.9"))
-			style += "#E8CBF6";  
-		else if (item.getSampleType().startsWith("Standard 9"))
-			style += "#E8CBF6";  
-		else if (item.getSampleType().startsWith("Blank"))
-			style += "#CEF7E8";  
-		else if (item.getSampleType().startsWith("Master Pool"))
-			style += "#F6CBCF";  
-		else if (item.getSampleType().startsWith("Pool.0"))
-			style += "#F6CBCF";  
-		else if (item.getSampleType().startsWith("Pool.1b"))
-			style += "#B3CDE0";  
-		else if (item.getSampleType().startsWith("Batch Pool.M1"))
-		        style += "#CBF6D2";	 
-		else if (item.getSampleType().startsWith("Pool.1"))
-		        style += "#CBF6D2";	 		
-		else if (item.getSampleType().startsWith("Batch Pool.M2"))
-			style += "#CBD9F6";  
-		else if (item.getSampleType().startsWith("Pool.2"))
-			style += "#CBD9F6";  
-		else if (item.getSampleType().startsWith("Batch Pool.M3"))
-			style += "#E8CBF6";  
-		else if (item.getSampleType().startsWith("Pool.3"))
-			style += "#E8CBF6";  
-		else if (item.getSampleType().startsWith("Batch Pool.M4"))
-			style += "#F6CBF2";  
-		else if (item.getSampleType().startsWith("Pool.4"))
-			style += "#F6CBF2";  
-		else if (item.getSampleType().startsWith("Batch Pool.M5"))
-			style += "#E9E9FF";
-		else if (item.getSampleType().startsWith("Pool.5"))
-			style += "#E9E9FF";
-		else if (item.getSampleType().startsWith("Other Pool.0"))
-			style += "#EFF6CB"; 
-		else if (item.getSampleType().startsWith("Pool.6"))
-			style += "#EFF6CB"; 
-		else if (item.getSampleType().startsWith("Other Pool.1"))
-			style += "#CBF6D2";
-		else if (item.getSampleType().startsWith("Pool.7"))
-			style += "#CBF6D2";
-		else if (item.getSampleType().startsWith("Other Pool.2"))
-			style += "#CBD9F6"; 
-		else if (item.getSampleType().startsWith("Pool.8"))
-			style += "#CBD9F6"; 
-		else if (item.getSampleType().startsWith("Other Pool.3"))
-			style += "#E8CBF6"; 
-		else if (item.getSampleType().startsWith("Pool.9"))
-			style += "#E8CBF6"; 
-		else if (item.getSampleType().startsWith("Reference 1 - urine"))
-			style += "#e9fb8c"; 
-		else if (item.getSampleType().startsWith("Reference 2 - urine"))
-			style += "#f7fed6"; 
-		else if (item.getSampleType().startsWith("Reference 1 - plasma"))
-			style += "#d8f4f8"; 
-		else if (item.getSampleType().startsWith("Reference 2 - plasma"))
-			style += "#ACE8F1"; 
-		// MotrPac Issue 422
-		// Issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Gastrocnemius, Exercise"))
-			style += "#FF92BB";
-		// issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Gastrocnemius, Sedentary"))
-			style += "#EEE0E5";
-		// Issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Liver, Exercise"))
-			style += "#BDA0CB";
-		// issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Liver, Sedentary"))
-			style += "#CDB5CD";
-		// Issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Adipose, Exercise"))
-			style += "#ECC8EC";
-		// issue 422
-	    else if (item.getSampleType().startsWith("MoTrPAC -   Adipose, Sedentary"))
-            style += "#EEA9B8";
-		// Issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Plasma, Exercise"))
-			style += "#DCA2CD";
-		// issue 422
-		else if (item.getSampleType().startsWith("MoTrPAC -   Plasma, Sedentary"))
-			style += "#D3BECF";
-		// issue 422
-		else if (item.getSampleType().startsWith("UM rat   plasma control"))
-			style += "#FFADB9";	
-		// issue 427
-		else if (item.getSampleType().startsWith("UM rat   liver control"))
-			style += "#EE799F";	
-		else if (item.getSampleType().startsWith("UM rat   adipose control"))
-			style += "#F6C9CC";	
-		else if (item.getSampleType().startsWith("UM rat   gastrocnemius control"))
-			style += "#D0A9AA";	
-		// issue 22
-		else if (item.getSampleType().startsWith("MoTrPAC -   Lung, Exercise"))
-			style += "#FFDF00";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Lung, Sedentary"))
-			style += "#D4AF37";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Kidney, Exercise"))
-			style += "#CFB53B";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Kidney, Sedentary"))
-			style += "#C5B358";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Heart, Exercise"))
-			style += "#E6BE8A";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Heart, Sedentary"))
-			style += "#DAA520";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Brown Adipose, Exercise"))
-			style += "#CD853F";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Brown Adipose, Sedentary"))
-			style += "#D2691E";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Hippocampus, Exercise"))
-			style += "#EEE8AA";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Hippocampus, Sedentary"))
-			style += "#FAFAD2";
-		// issue 126
-		else if (item.getSampleType().startsWith("MoTrPAC -   Muscle-Human : Male"))
-			style += "#66FF99";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Muscle-Human : Female"))
-			style += "#CC99FF";
-		else if (item.getSampleType().startsWith("UM Human muscle control"))
-			style += "#CCFFFF";
-		// issue 193
-		else if (item.getSampleType().startsWith("MoTrPAC -   Plasma-Human: Male"))
-		//	style += "#CCFAFf";
-			// issue 313
-			style += "#B1BC55";
-		else if (item.getSampleType().startsWith("MoTrPAC -   Plasma-Human: Female"))
-			style += "#CCF0FF";
-		//else if (item.getSampleType().startsWith("Injection - plasma   (R00CHRPL1-Pre)") )
-		else if (item.getSampleType().startsWith("Injection"))
-			style += "#c8a2c8";
-			// color will be good choice when pool1b is no longer use 
-		else if (item.getSampleType().startsWith("Adi RefStdA"))
-			style += "#EADDCA";
-		else if (item.getSampleType().startsWith("Adi RefStdB"))
-			style += "#FFBF00";
-		else if (item.getSampleType().startsWith("Adi RefStdC"))
-			style += "#FBCEB1";
-		else if (item.getSampleType().startsWith("Adi RefStdD"))
-			style += "#F5F5DC";
-		else if (item.getSampleType().startsWith("Adi RefStdE"))
-			style += "#E1C16E";
-			
-		else if (item.getRepresentsUserDefinedControl())
-			style += "#e0b3dd";	
+	{     
+	String style = "background :";
+	String name = item.getSampleType().trim();
+//	if (item.getSampleType().contains("Blank"))
+//		style += "#CEF7E8";  
+	if (item.getSampleType().contains("Pooled Plasma"))
+		style += "#F6CDCB"; 
+	else if (item.getSampleType().contains("Standard Mix"))
+		style += "#CBE3F6";  
+	else if (item.getSampleType().contains("Test Pool"))
+		style += "#D9F6CB";  
+	else if (item.getSampleType().contains("Neat Blank"))
+		style += "#CEF7E8";    
+	// issue 255
+	else if (item.getSampleType().contains("Process Blank") || item.getSampleType().contains("Injection - Process Blank"))
+		style += "#FEE8DD";  
+	else if (item.getSampleType().contains("RC Plasma"))
+		style += "#CBE3F6";  
+	else if (item.getSampleType().contains("Red Cross"))
+		style += "#CBE3F6";  
+	else if (item.getSampleType().contains("Matrix Blank"))
+		style += "#CBE3F6";  	
+	else if (item.getSampleType().contains("Solvent Blank"))
+		style += "#D9F6CB";  
+	else if (item.getSampleType().contains("Standard.0"))
+		style += "#EFF6CB";  
+	else if (item.getSampleType().contains("Standard 0"))
+		style += "#EFF6CB";  
+	else if (item.getSampleType().contains("Standard.10"))
+		style += "#F6CBF2";  
+	else if (item.getSampleType().contains("Standard 10"))
+		style += "#F6CBF2";  
+	else if (item.getSampleType().contains("Standard.11"))
+		style += "#E9E9FF";
+	else if (item.getSampleType().contains("Standard 11"))
+		style += "#E9E9FF";
+	else if (item.getSampleType().contains("Standard.1"))
+		style += "#E9E9FF";  
+	else if (item.getSampleType().contains("Standard 1"))
+		style += "#E9E9FF";  
+	else if (item.getSampleType().contains("Standard.2"))
+		style += "#CBD9F6";  
+	else if (item.getSampleType().contains("Standard 2"))
+		style += "#CBD9F6";  
+	else if (item.getSampleType().contains("Standard.3"))
+		style += "#E8CBF6";  
+	else if (item.getSampleType().contains("Standard 3"))
+		style += "#E8CBF6";  
+	else if (item.getSampleType().contains("Standard.4"))
+		style += "#F6CBF2";  
+	else if (item.getSampleType().contains("Standard 4"))
+		style += "#F6CBF2";  
+	else if (item.getSampleType().contains("Standard.5"))
+		style += "#E9E9FF";
+	else if (item.getSampleType().contains("Standard 5"))
+		style += "#E9E9FF";
+	else if (item.getSampleType().contains("Standard.6"))
+		style += "#EFF6CB";  
+	else if (item.getSampleType().contains("Standard 6"))
+		style += "#EFF6CB";  
+	else if (item.getSampleType().contains("Standard.7"))
+		style += "#CBF6D2";  
+	else if (item.getSampleType().contains("Standard 7"))
+		style += "#CBF6D2";  
+	else if (item.getSampleType().contains("Standard.8"))
+		style += "#CBD9F6";  
+	else if (item.getSampleType().contains("Standard 8"))
+		style += "#CBD9F6";  
+	else if (item.getSampleType().contains("Standard.9"))
+		style += "#E8CBF6";  
+	else if (item.getSampleType().contains("Standard 9"))
+		style += "#E8CBF6";  
+	else if (item.getSampleType().contains("Blank"))
+		style += "#CEF7E8";  
+	else if (item.getSampleType().contains("Master Pool") || item.getSampleType().contains("Injection - pool   (CS00000MP-Pre)"))
+		style += "#F6CBCF";  
+	else if (item.getSampleType().contains("Pool.0"))
+		style += "#F6CBCF";  
+	else if (item.getSampleType().contains("Pool.1b"))
+		style += "#B3CDE0";  
+	else if (item.getSampleType().contains("Batch Pool.M1") || item.getSampleType().contains("Injection - pool   (CS000BPM1-Pre)"))
+	        style += "#CBF6D2";	 
+	else if (item.getSampleType().contains("Pool.1"))
+	        style += "#CBF6D2";	 		
+	else if (item.getSampleType().contains("Batch Pool.M2") || item.getSampleType().contains("Injection - pool   (CS000BPM2-Pre)"))
+		style += "#CBD9F6";  
+	else if (item.getSampleType().contains("Pool.2"))
+		style += "#CBD9F6";  
+	else if (item.getSampleType().contains("Batch Pool.M3") || item.getSampleType().contains("Injection - pool   (CS000BPM3-Pre)"))
+		style += "#E8CBF6";  
+	else if (item.getSampleType().contains("Pool.3"))
+		style += "#E8CBF6";  
+	else if (item.getSampleType().contains("Batch Pool.M4") || item.getSampleType().contains("Injection - pool   (CS000BPM4-Pre)"))
+		style += "#F6CBF2";  
+	else if (item.getSampleType().contains("Pool.4"))
+		style += "#F6CBF2";  
+	else if (item.getSampleType().contains("Batch Pool.M5") || item.getSampleType().contains("Injection - pool   (CS000BPM5-Pre)"))
+		style += "#E9E9FF";
+	else if (item.getSampleType().contains("Pool.5"))
+		style += "#E9E9FF";
+	else if (item.getSampleType().contains("Other Pool.0"))
+		style += "#EFF6CB"; 
+	else if (item.getSampleType().contains("Pool.6"))
+		style += "#EFF6CB"; 
+	else if (item.getSampleType().contains("Other Pool.1"))
+		style += "#CBF6D2";
+	else if (item.getSampleType().contains("Pool.7"))
+		style += "#CBF6D2";
+	else if (item.getSampleType().contains("Other Pool.2"))
+		style += "#CBD9F6"; 
+	else if (item.getSampleType().contains("Pool.8"))
+		style += "#CBD9F6"; 
+	else if (item.getSampleType().contains("Other Pool.3"))
+		style += "#E8CBF6"; 
+	else if (item.getSampleType().contains("Pool.9"))
+		style += "#E8CBF6"; 
+	else if (item.getSampleType().contains("Reference 1 - urine")  || item.getSampleType().contains("Injection - urine"))
+		style += "#e9fb8c"; 
+	else if (item.getSampleType().contains("Reference 2 - urine"))
+		style += "#f7fed6"; 
+	else if (item.getSampleType().contains("Reference 1 - plasma") ||  item.getSampleType().contains("Injection - plasma"))
+		style += "#d8f4f8"; 
+	else if (item.getSampleType().contains("Reference 2 - plasma"))
+		style += "#ACE8F1"; 
+	// MotrPac Issue 422
+	// Issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Gastrocnemius, Exercise"))
+		style += "#FF92BB";
+	// issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Gastrocnemius, Sedentary"))
+		style += "#EEE0E5";
+	// Issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Liver, Exercise"))
+		style += "#BDA0CB";
+	// issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Liver, Sedentary"))
+		style += "#CDB5CD";
+	// Issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Adipose, Exercise"))
+		style += "#ECC8EC";
+	// issue 422
+    else if (item.getSampleType().contains("MoTrPAC -   Adipose, Sedentary"))
+        style += "#EEA9B8";     
+	// Issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Plasma, Exercise"))
+		style += "#DCA2CD";
+	// issue 422
+	else if (item.getSampleType().contains("MoTrPAC -   Plasma, Sedentary"))
+		style += "#D3BECF";
+	// issue 422
+	else if (item.getSampleType().contains("UM rat   plasma control"))
+		style += "#FFADB9";	
+	// issue 427
+	else if (item.getSampleType().contains("UM rat   liver control"))
+		style += "#EE799F";	
+	else if (item.getSampleType().contains("UM rat   adipose control"))
+		style += "#F6C9CC";	
+	else if (item.getSampleType().contains("UM rat   gastrocnemius control"))
+		style += "#D0A9AA";	
+	// issue 22
+	else if (item.getSampleType().contains("MoTrPAC -   Lung, Exercise"))
+		style += "#FFDF00";
+	else if (item.getSampleType().contains("MoTrPAC -   Lung, Sedentary"))
+		style += "#D4AF37";
+	else if (item.getSampleType().contains("MoTrPAC -   Kidney, Exercise"))
+		style += "#CFB53B";
+	else if (item.getSampleType().contains("MoTrPAC -   Kidney, Sedentary"))
+		style += "#C5B358";
+	else if (item.getSampleType().contains("MoTrPAC -   Heart, Exercise"))
+		style += "#E6BE8A";
+	else if (item.getSampleType().contains("MoTrPAC -   Heart, Sedentary"))
+		style += "#DAA520";
+	else if (item.getSampleType().contains("MoTrPAC -   Brown Adipose, Exercise"))
+		style += "#CD853F";
+	else if (item.getSampleType().contains("MoTrPAC -   Brown Adipose, Sedentary"))
+		style += "#D2691E";
+	else if (item.getSampleType().contains("MoTrPAC -   Hippocampus, Exercise"))
+		style += "#EEE8AA";
+	else if (item.getSampleType().contains("MoTrPAC -   Hippocampus, Sedentary"))
+		style += "#FAFAD2";
+	// issue 126
+	else if (item.getSampleType().contains("MoTrPAC -   Muscle-Human : Male"))
+		style += "#66FF99";
+	else if (item.getSampleType().contains("MoTrPAC -   Muscle-Human : Female"))
+		style += "#CC99FF";
+	else if (item.getSampleType().contains("UM Human muscle control"))
+		style += "#CCFFFF";
+	// issue 193
+	else if (item.getSampleType().contains("MoTrPAC -   Plasma-Human: Male"))
+	//	style += "#CCFAFf";
+		// issue 313
+		style += "#B1BC55";
+	else if (item.getSampleType().contains("MoTrPAC -   Plasma-Human: Female"))
+		style += "#CCF0FF";
+
+		// color will be good choice when pool1b is no longer use 
+	else if (item.getSampleType().contains("Adi RefStdA"))
+		style += "#EADDCA";
+	else if (item.getSampleType().contains("Adi RefStdB"))
+		style += "#FFBF00";
+	else if (item.getSampleType().contains("Adi RefStdC"))
+		style += "#FBCEB1";
+	else if (item.getSampleType().contains("Adi RefStdD"))
+		style += "#F5F5DC";
+	else if (item.getSampleType().contains("Adi RefStdE"))
+		style += "#E1C16E";
 		
-		else
-			style += "#eaeef2";  
-		return style;		
-		}
+	else if (item.getRepresentsUserDefinedControl())
+		style += "#e0b3dd";	
+	
+	else
+		style += "#eaeef2";    
+	
+	return style;	    	
+	}
 
 	//issue 394 get rid of dead routines buildDynamicHOverMessageBehavior, trimtags
 	
